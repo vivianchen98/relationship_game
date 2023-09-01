08c10d67cb827e29b67804e041abd7441dded345
vivianchen98 <shenghui.chen@utexas.edu>
2023-09-01 13:54:50 -0500

    update

--- game_solvers/entropy_nash_solver_jump.jl
+++ game_solvers/entropy_nash_solver_jump.jl
@@ -1,21 +1,43 @@
 using JuMP, LinearAlgebra, Ipopt
+using Zygote, ChainRulesCore
 
-# stable softmax
-function softmax(arr; θ=1)
-    e = exp.((arr .- maximum(arr)) * θ)
-    return e./ sum(e)
+
+function prob_prod(x, player_indices, cartesian_indices)
+    out = zeros(size(cartesian_indices))
+    for idx in cartesian_indices
+        out[idx] = prod(x[i][idx[i]] for i in player_indices)
+    end
+    return out
 end
 
 function lazy_J_except_i(i, x, u)
     num_actions = size(u[1])[1]
+    N = length(x)
     cost = zeros(num_actions)
     for ai in 1:num_actions
         x_alt = copy(x)
         pure_strat_i = zeros(num_actions)
         pure_strat_i[ai] = 1
         x_alt[i] = pure_strat_i
-        full_list = [s for s in 1:length(x_alt)]
-        cost[ai] = sum(u[i] .* prob_prod(x_alt, full_list, CartesianIndices(u[i])))
+        # full_list = [s for s in 1:length(x_alt)]
+        # cost[ai] = sum(u[i] .* prob_prod(x_alt, full_list, CartesianIndices(u[i])))
+        cost[ai] = sum(u[i] .* prob_prod(x_alt, collect(1:N), CartesianIndices(u[i])))
+    end
+    return cost
+end
+
+function lazy_J_except_i_jump(i, x, u, N, num_actions)
+    # num_actions = size(u[1])[1]
+    # N = length(x)
+    cost = zeros(num_actions)
+    for ai in 1:num_actions
+        x_alt = copy(x)
+        pure_strat_i = zeros(num_actions)
+        pure_strat_i[ai] = 1
+        x_alt[i] = pure_strat_i
+        # full_list = [s for s in 1:length(x_alt)]
+        # cost[ai] = sum(u[i] .* prob_prod(x_alt, full_list, CartesianIndices(u[i])))
+        cost[ai] = sum(u[i] .* prob_prod(x_alt, collect(1:N), CartesianIndices(u[i])))
     end
     return cost
 end
@@ -28,19 +50,29 @@ function solve_entropy_nash_general(u, λ)
     num_actions = size(u[1])[1]
 
     # variables
-    @variable(model, x[1:N; 1:num_actions] >= 0)
+    @variable(model, x[1:N, 1:num_actions] >= 0)
 
 
     # slack variable
-    @variable(model, p[1:N])
+    @variable(model, p[1:N, 1:num_actions])
+    @variable(model, cost[1:N, 1:num_actions])
 
-    # constraints
+
+    # ??? cost = J_except_i(x,u[i])
     for i in 1:N
-        @NLconstraint(model, x[i, :] - softmax(- lazy_J_except_i(i, x, u) ./ λ) == p[i])
+        # @constraint(model, cost[i, :] == [sum(u[i] .* out[i, :, :, :]) for i in 1:N])
+
+        sum(prod(x[i, ]) .* u[i, j] for j in 1:num_actions)
+
+    end
+
+    # softmax(-cost/λ)
+    for i in 1:N, j in 1:num_actions
+        @NLconstraint(model, x[i, j] - (exp(-cost[i,j]/λ) / sum(exp(-cost[i,j]/λ) for j in 1:num_actions)) == p[i, j])
     end
 
     # objective
-    @objective(m, Min, sum(p[i]*p[i] for i in 1:N))
+    @objective(m, Min, sum(sum(p[i, :] .* p[i, :]) for i in 1:N))
 
 
     optimize!(m)
